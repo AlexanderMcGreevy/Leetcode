0811cf0a4b3a896d97b102a98745f5f394545e31
Alexander McGreevy <alexander.mcgreevy@icloud.com>
2026-01-16 18:14:24 -0500

    Time: 7 ms (18.28%) | Memory: 12.5 MB (64.57%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        pairs = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch in pairs.values():  # opening bracket
+                stack.append(ch)
+            elif ch in pairs:  # closing bracket
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+
+        return len(stack) == 0
