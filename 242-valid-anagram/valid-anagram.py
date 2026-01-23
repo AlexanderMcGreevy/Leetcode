f11c948d59492522c2f65d1ee99de12a92cfc2c0
Alexander McGreevy <alexander.mcgreevy@icloud.com>
2026-01-23 16:42:21 -0500

    Time: 168 ms (5.01%) | Memory: 12.6 MB (82.84%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        temp = t
+        for x in s:
+            if x in temp:
+                temp=temp.replace(x,"",1)
+            else:
+                return False
+        if len(temp) == 0:
+            return True
+        return False
+        
