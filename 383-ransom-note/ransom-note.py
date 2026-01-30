5ff166fa8f87a859e93584d562ae4cf22a6df31b
Alexander McGreevy <alexander.mcgreevy@icloud.com>
2026-01-30 16:14:58 -0500

    Time: 23 ms (84.92%) | Memory: 12.6 MB (98.47%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        magazine=list(magazine)
+        for x in ransomNote:
+            if x in magazine:
+                magazine.remove(x)
+            else: return False
+        return True
