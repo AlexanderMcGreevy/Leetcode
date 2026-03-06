814adce9d8e50d4b6e11281fb694532dbe7a5289
Alexander McGreevy <alexander.mcgreevy@icloud.com>
2026-03-06 17:07:40 -0500

    Time: 103 ms (81.28%) | Memory: 21.9 MB (69.56%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,24 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(mat), len(mat[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        queue = deque()
+        height = len(mat)
+        width = len(mat[0])
+        q=[]
 
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(height):
+            for j in range(width):
                 if mat[i][j] == 0:
-                    queue.append((i, j))
+                    q.append((i,j))
                 else:
-                    mat[i][j] = float('inf')
-
-        while queue:
-            row, col = queue.popleft()
-
-            for dr, dc in directions:
-                new_row, new_col = row + dr, col + dc
-
-                if 0 <= new_row < rows and 0 <= new_col < cols and mat[new_row][new_col] > mat[row][col] + 1:
-                    mat[new_row][new_col] = mat[row][col] + 1
-                    queue.append((new_row, new_col))
-
-        return mat        
+                    mat[i][j] = "#"
+        
+        for x,y in q:
+            for dx,dy in (1,0),(0,1),(-1,0),(0,-1):
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < height and 0 <= ny < width and mat[nx][ny] == "#":
+                    mat[nx][ny] = mat[x][y] + 1
+                    q.append((nx,ny))
+        return mat
+        
